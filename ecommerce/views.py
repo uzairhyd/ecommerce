bba367dd93e0d6e21bb3dab387667eb23c766d4c
dulton <uzairhyd89@gmail.com>
2023-01-21 00:09:03 +0530

    First commit

---
+++ ecommerce/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import ContactForm
+
+def home_page(request):
+    context = {
+        'title': "Hello World",
+        "content": "Welcome to the home page"
+    }
+    return render(request, 'home_page.html', context)
+
+def about_page(request):
+    context = {
+        'title': "About Page",
+        "content": "Welcome to the about page"
+    }
+    return render(request, 'home_page.html', context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        'title': "Contact Page",
+        "content": "Welcome to the contact page",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    return render(request, 'contact/view.html', context)
