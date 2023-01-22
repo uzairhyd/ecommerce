dd9e374bbc1d5435d9004c42de1bc24b3fb58bdd
uzairhyd <uzairhyd89@gmail.com>
2023-01-22 17:26:23 +0530

    Detail view done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,7 +23,9 @@ from .views import home_page, about_page, contact_page, login_page, register_pag
 
 from products.views import (
     ProductListView,
-    product_list_view
+    product_list_view,
+    ProductDetailView,
+    product_detail_view
     )
 
 urlpatterns = [
@@ -35,6 +37,8 @@ urlpatterns = [
 
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
+    path('products/<int:pk>/', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>/', product_detail_view),
 
     path('admin/', admin.site.urls),
 ]
