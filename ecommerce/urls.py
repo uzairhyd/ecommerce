1dfa15405f68bc6aa797060df8dc08761f3f0cfa
uzairhyd <uzairhyd89@gmail.com>
2023-01-22 17:08:57 +0530

    added the products list view

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,12 +21,21 @@ from django.urls import path
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
+from products.views import (
+    ProductListView,
+    product_list_view
+    )
+
 urlpatterns = [
     path('', home_page),
     path('about/', about_page),
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+
+    path('products/', ProductListView.as_view()),
+    path('products-fbv/', product_list_view),
+
     path('admin/', admin.site.urls),
 ]
 
